734a5637c03159d21db2e53f91fb415ef8f57fe4
Neelam naga satya sri prasad <157829337+prasad-ns@users.noreply.github.com>
2025-03-26 00:14:19 +0530

    Time: 35 ms (100.00%) | Memory: 118.1 MB (70.61%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.java
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.java
@@ -1,37 +1,34 @@
-class Solution {
-    public boolean checkValidCuts(int n, int[][] rectangles) {
-        //we will extract the intervals of the x
-        int[][] x = new int[rectangles.length][2];
-        for (int i = 0; i < rectangles.length; i++) {
-            x[i][0] = rectangles[i][0];
-            x[i][1] = rectangles[i][2];
+public class Solution {
+    private static final int MASK = (1 << 30) - 1;
+
+    public boolean checkValidCuts(int m, int[][] rectangles) {
+        int n = rectangles.length;
+        long[] start = new long[n];
+        for (int i = 0; i < n; i++) {
+            start[i] = ((long) rectangles[i][1] << 32) + rectangles[i][3];
         }
-        //we will extrtact the y intervals here
-        int[][] y = new int[rectangles.length][2];
-        for (int i = 0; i < rectangles.length; i++) {
-            y[i][0] = rectangles[i][1];
-            y[i][1] = rectangles[i][3];
+        Arrays.sort(start);
+        if (validate(start)) {
+            return true;
         }
-        Arrays.sort(x, (a, b) -> Integer.compare(a[0], b[0]));
-        Arrays.sort(y, (a, b) -> Integer.compare(a[0], b[0]));
-        return Math.max(
-            countNonOverlapping(x),
-            countNonOverlapping(y)
-        ) >= 3;
+        for (int i = 0; i < n; i++) {
+            start[i] = ((long) rectangles[i][0] << 32) + rectangles[i][2];
+        }
+        Arrays.sort(start);
+        return validate(start);
     }
-    private int countNonOverlapping(int[][] intervals) {
-        int count = 0;
-        int prevEnd = -1;
-        for (int[] interval : intervals) {
-            int start = interval[0];
-            int end = interval[1];
-            if (prevEnd <= start) {
-                count++;
-                prevEnd = end;
-            } else {
-                prevEnd = Math.max(prevEnd, end); 
+
+    private boolean validate(long[] arr) {
+        int cut = 0;
+        int n = arr.length;
+        int max = (int) arr[0] & MASK;
+        for (int i = 0; i < n; i++) {
+            int start = (int) (arr[i] >> 32);
+            if (start >= max && ++cut == 2) {
+                return true;
             }
+            max = Math.max(max, (int) (arr[i] & MASK));
         }
-        return count;
+        return false;
     }
 }
