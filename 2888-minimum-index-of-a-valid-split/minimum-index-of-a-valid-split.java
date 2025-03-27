8ee2bae92077d26233967a48d2ed5fed7ff58f72
Neelam naga satya sri prasad <157829337+prasad-ns@users.noreply.github.com>
2025-03-27 23:30:44 +0530

    Time: 10 ms (97.44%) | Memory: 55 MB (99.36%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -1,40 +1,34 @@
-import java.util.*;
-
 class Solution {
-    public int minimumIndex(List<Integer> nums) {
-        int n = nums.size();
-        
-        // Step 1: Find the dominant element in the whole array
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
-        }
-        
-        int dominant = -1;
-        int maxCount = 0;
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
-            if (entry.getValue() * 2 > n) {
-                dominant = entry.getKey();
-                maxCount = entry.getValue();
-                break;
+    public int minimumIndex(List<Integer> a) {
+        int count = 0;
+        int num = -1;
+        int n = a.size();
+
+        for(int i = 0; i < a.size(); i++){
+            if(count == 0){
+                num = a.get(i);
+                count++;
+            }
+            else{
+                if(num == a.get(i)) ++count;
+                else --count;
             }
         }
-        
-        if (dominant == -1) return -1;
-        
-        // Step 2: Try to find the minimum index of a valid split
-        int leftCount = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (nums.get(i) == dominant) {
-                leftCount++;
+
+        count = 0;
+        for(int i = 0; i < n; i++) if(num == a.get(i)) ++count;
+
+        int curr = 0;
+        for(int i = 0; i < n; i++){
+            if(a.get(i) == num){
+                ++curr;
             }
-            int rightCount = maxCount - leftCount;
-            
-            if (leftCount * 2 > (i + 1) && rightCount * 2 > (n - i - 1)) {
-                return i;
-            }
+
+            if(2 * curr > (i + 1) && 2 * (count - curr) > n - i - 1) return i;
         }
-        
+
         return -1;
+
+
     }
 }
