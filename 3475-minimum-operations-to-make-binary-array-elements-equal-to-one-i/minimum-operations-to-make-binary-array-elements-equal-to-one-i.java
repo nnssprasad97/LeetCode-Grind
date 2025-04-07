1687801ecbdf3e985c48f40d3de336c906c3b2d2
Neelam naga satya sri prasad <157829337+nnssprasad97@users.noreply.github.com>
2025-04-07 20:20:20 +0530

    Time: 6 ms (99.81%) | Memory: 57.2 MB (68.25%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,18 +1,15 @@
 class Solution {
     public int minOperations(int[] nums) {
-            int operations = 0;
-            int n = nums.length;
-        for (int i = 0; i < n - 2; i++) {
-            if (nums[i] == 0) {
-                nums[i] ^= 1;
-                nums[i + 1] ^= 1;
-                nums[i + 2] ^= 1;
-                operations++;
+        int count = 0;
+        for(int i = 0;i < nums.length - 2;i++){
+            if(nums[i] == 0){
+                nums[i] = 1 - nums[i];
+                nums[i + 1] = 1 - nums[i + 1];
+                nums[i + 2] = 1 - nums[i + 2];
+                count += 1;
             }
         }
-        for (int num : nums) {
-            if (num == 0)return -1;
-        }
-        return operations;
+        if(nums[nums.length - 1] == 0 || nums[nums.length - 2] == 0)return -1; 
+        return count;
     }
 }
