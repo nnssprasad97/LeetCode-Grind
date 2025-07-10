f12511eac6bd27235958e9ca5530a8a72c279b4f
Neelam naga satya sri prasad <157829337+nnssprasad97@users.noreply.github.com>
2025-07-10 20:38:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (27.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,15 +1,11 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1)return n;
-        //return fib(n-1)+fib(n-2);
-        //also this can be solved by
-        int firstval=0,secondval=1,finalval=0;
-        while(n>=2){
-            finalval=firstval+secondval;
-            firstval=secondval;
-            secondval=finalval;
-            n--;
-        }
-        return finalval;
+        if(n<=0)return 0;
+       if(n==1 || n==2)return 1;
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 }
