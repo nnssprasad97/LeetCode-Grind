bc2607a4dd9dd4e484574cca575de150f798b12a
Neelam naga satya sri prasad <157829337+nnssprasad97@users.noreply.github.com>
2025-09-22 22:46:46 +0530

    Time: 2 ms (18.15%) | Memory: 45.8 MB (7.97%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.java
@@ -0,0 +1,28 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer>list=new ArrayList<>();
+        if(root==null)return list;
+        for(Node n:root.children)list.addAll(postorder(n));
+        list.add(root.val);
+        return list;
+    }
+}
