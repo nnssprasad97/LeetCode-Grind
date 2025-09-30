00a704d79e1a975b12ce2a3ff09de3ccdd0353ae
Neelam naga satya sri prasad <157829337+nnssprasad97@users.noreply.github.com>
2025-09-30 23:28:01 +0530

    Time: 40 ms (98.11%) | Memory: 44.8 MB (45.68%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
@@ -1,10 +1,13 @@
 class Solution {
     public int triangularSum(int[] nums) {
-        int n=nums.length;
-        while(n>1){
-            for(int i=0;i<n-1;i++) nums[i] = (nums[i] + nums[i + 1]) % 10;
-            n--;
+        return func(nums, nums.length);
+    }
+
+    private int func(int[] arr, int n){
+        if(n==1) return arr[0]; //bc
+        for(int i=0;i<n-1;i++){
+            arr[i]=(arr[i]+arr[i+1]) %10; //already shrunk by -1 (size of the array)
         }
-        return nums[0];
+        return func(arr, n-1); //cuz after every step array size will dec by 1
     }
 }
