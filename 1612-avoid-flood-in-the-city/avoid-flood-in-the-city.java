15f7708b6e5322337250df5483eec479ee44e9bd
Neelam naga satya sri prasad <157829337+nnssprasad97@users.noreply.github.com>
2025-10-07 23:35:32 +0530

    Time: 63 ms (94.52%) | Memory: 62.2 MB (46.58%) - LeetSync

---
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] avoidFlood(int[] rains) {
+        int n = rains.length;
+        int[] ans = new int[n];
+        Map<Integer, Integer> lakeLast = new HashMap<>();
+        TreeSet<Integer> dryDays = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            if (rains[i] == 0) {
+                dryDays.add(i);
+                ans[i] = 1;
+            } else {
+                int lake = rains[i];
+                ans[i] = -1;
+                if (lakeLast.containsKey(lake)) {
+                    Integer dryDay = dryDays.higher(lakeLast.get(lake));
+                    if(dryDay==null)return new int[0];
+                    ans[dryDay] = lake;
+                    dryDays.remove(dryDay);
+                }
+                lakeLast.put(lake, i);
+            }
+        }
+        return ans;
+    }
+}
