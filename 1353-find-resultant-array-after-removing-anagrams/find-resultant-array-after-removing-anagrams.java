c1824f596aaf198ab0578e4a430ae42cce68843d
Neelam naga satya sri prasad <157829337+nnssprasad97@users.noreply.github.com>
2025-10-13 22:10:02 +0530

    Time: 1 ms (99.78%) | Memory: 45 MB (22.27%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -1,17 +1,34 @@
-import java.util.*;
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        List<String>result=new ArrayList<>();
-        result.add(words[0]);
-        for(int i=1;i<words.length;i++)if(!isAnagram(words[i],words[i-1]))result.add(words[i]);
-        return result;
+        List<String> list= new ArrayList<>();
+        list.add(words[0]);
+        for(int i=1;i<words.length;i++){
+         
+                if(!isAnagram(words[i],words[i-1])){
+                    list.add(words[i]);
+                }
+            }
+        
+        return list;
+
+
+        
     }
-    private boolean isAnagram(String s1,String s2){
-        if(s1.length()!=s2.length())return false;
-        char[] a=s1.toCharArray();
-        char[] b=s2.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b);
+    public boolean isAnagram(String s, String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[]count= new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        for(int c:count){
+            if(c!=0){
+                return false;
+            }
+        }
+        return true;
     }
+   
 }
