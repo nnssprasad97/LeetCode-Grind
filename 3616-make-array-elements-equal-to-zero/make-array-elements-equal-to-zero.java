4ef33c0acb40bc0c4cfb69ae8b6f8184072f4c2e
Neelam naga satya sri prasad <157829337+nnssprasad97@users.noreply.github.com>
2025-10-28 08:09:42 +0530

    Time: 1 ms (83.13%) | Memory: 41.8 MB (98.19%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.java
@@ -1,24 +1,23 @@
 class Solution {
     public int countValidSelections(int[] nums) {
-        int n = nums.length, res = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) continue;
-            for (int d : new int[]{-1, 1}) {
-                int[] arr = nums.clone();
-                int curr = i, dir = d;
-                while (curr >= 0 && curr < n) {
-                    if (arr[curr] == 0) curr += dir;
-                    else {
-                        arr[curr]--;
-                        dir = -dir;
-                        curr += dir;
-                    }
-                }
-                boolean ok = true;
-                for (int v : arr) if (v != 0) { ok = false; break; }
-                if (ok) res++;
+
+        int count = 0, len = nums.length;
+        int sum = 0;
+        for (int num : nums) sum += num;
+
+        int halfSum = 0;
+
+        for (int i = 0; i < len; i++) {
+            halfSum += nums[i];
+            if (nums[i] == 0) {
+
+                if (2 * halfSum == sum) {
+                    count += 2;
+                } else if (Math.abs(sum - 2*halfSum) == 1) {
+                    count++;
+                } 
             }
         }
-        return res;
+        return count;
     }
 }
