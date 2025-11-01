b5371d88a4b9e30fbebf47ab6c3dafb1db38b613
Neelam naga satya sri prasad <157829337+nnssprasad97@users.noreply.github.com>
2025-11-01 23:59:29 +0530

    Time: 18 ms (93.82%) | Memory: 145.6 MB (5.52%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -8,28 +8,42 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode modifiedList(int[] nums, ListNode head) {
+//         ListNode dummy = new ListNode(0, head);
+//         ListNode cur = dummy;
+//         int[] cnt = new int[(int)Math.pow(10, 5) + 1];
+//         for (int num : nums) {
+//             cnt[num] ++;
+//         }
+//         while (cur.next != null) {
+//             if (cnt[cur.next.val] == 1) {
+//                 cur.next = cur.next.next;
+//             } else {
+//                 cur = cur.next;
+//             }
+//         }
+//         return dummy.next;
+//     }
+// }
+
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        Set<Integer> set = new HashSet<>();
-        for(int x: nums)set.add(x);
-
-        if(head == null) return null;
-        while(head!=null && set.contains(head.val))
-            head = head.next;
-
-        if(head == null) return null;
-        var current = head;
-
-        while(current.next!=null){
-            if(set.contains(current.next.val)){
-               current.next = current.next.next; 
-            }
-            else{   
-                current = current.next;
-            }
+        Set<Integer> set = new HashSet<>(nums.length, 1);
+        for (int x : nums) {
+            set.add(x);
         }
 
-        return head;
+        ListNode dummy = new ListNode(0, head);
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            ListNode nxt = cur.next;
+            if (set.contains(nxt.val)) {
+                cur.next = nxt.next;
+            } else {
+                cur = nxt;
+            }
+        }
+        return dummy.next;
     }
-
 }
